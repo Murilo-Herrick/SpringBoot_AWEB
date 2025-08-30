f7b8a2fe8d025a76e17bf3907800ee960479b3b3
Murilo-Herrick <muriloherrick@gmail.com>
2025-08-29 22:51:20 -0300

    Aula - 03

---
+++ Aula-03/to-do-list/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+	INSERT INTO TODO (title, created_at, deadline, finished_at) VALUES
+		('Estudar Spring Boot', TIMESTAMP '2025-08-27 10:00:00', DATE '2025-09-05', NULL),
+		('Fazer exercícios de Java', TIMESTAMP '2025-08-27 11:00:00', DATE '2025-09-10', NULL),
+		('Preparar aula de UML', TIMESTAMP '2025-08-26 14:30:00', DATE '2025-09-02', DATE '2025-08-27'),
+		('Revisar projeto no GitHub', TIMESTAMP '2025-08-25 09:15:00', DATE '2025-09-01', NULL),
+		('Estudar banco de dados H2', TIMESTAMP '2025-08-24 16:45:00', DATE '2025-09-03', DATE '2025-08-26');
